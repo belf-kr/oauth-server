a185dd002abe0cd98e58c7cf7d9b8088082c4ef0
박경은 <ruddms936@naver.com>
2021-06-02 00:15:50 +0900

    add: 프로그램 버전 명 추가

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"oauth-server/controller"
 	"oauth-server/controller/token"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	version = "v0.1.0"
+)
+
+func init() {
+	fmt.Printf("version: %s\n", version)
+}
+
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -16,5 +26,6 @@ func main() {
 	})
 	r.GET("/login", controller.Login)
 	r.GET("/token/valid", token.Valid)
+
 	r.Run(":3000")
 }
