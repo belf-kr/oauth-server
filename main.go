2e617add5e964107a4c5a4a3e6276a4287cb0451
Kyungeun Park <63892989+parkgang@users.noreply.github.com>
2021-06-06 18:19:44 +0900

    기본적인 web server 구축 (#5)
    * up: 의존성
    
    * add: 테스트로 토큰 검사할 수 있는 컨트롤러
    
    * add: api spec
    
    * up: 로그인 사용자 반환값 변경
    
    * add: 토큰 값을 검사하는 res 추가
    
    * fix: 항상 이미지 pull 하도록 수정 (#4)
    
    * up: 사용하지 않는 의존성을 제거
    
    * add: 프로그램 버전 명 추가
    
    * up: 안내메시지 친화적으로 변경
    
    * up: 리소스 리미트 추가
    
    * docs: 설명 추가
    
    * up: 웹 서버 상태를 알 수 있는 기본 컨트롤러 추가

--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"oauth-server/controller"
+	"oauth-server/controller/token"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	name    = "oauth-server"
+	version = "0.1.0"
+)
+
+func init() {
+	fmt.Printf("version: %s\n", version)
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	r.GET("/", func(c *gin.Context) {
+		resData := []byte(name)
+		c.Data(http.StatusOK, "text/html; charset=utf-8", resData)
 	})
+	r.GET("/ping", func(c *gin.Context) {
+		resData := []byte(`OK`)
+		c.Data(http.StatusOK, "text/html; charset=utf-8", resData)
+	})
+	r.GET("/version", func(c *gin.Context) {
+		resData := []byte(version)
+		c.Data(http.StatusOK, "text/html; charset=utf-8", resData)
+	})
+	r.GET("/env", func(c *gin.Context) {
+		resData := os.Environ()
+		c.JSON(http.StatusOK, resData)
+	})
+
 	r.GET("/login", controller.Login)
+	r.GET("/token/valid", token.Valid)
+
 	r.Run(":3000")
 }
