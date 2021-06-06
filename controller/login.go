e9125ed2bbd611baeebe80befa46966f030512ba
Kyungeun Park <63892989+parkgang@users.noreply.github.com>
2021-06-06 18:19:44 +0900

    기본적인 web server 구축 (#5)
    * up: 의존성
    
    * add: 테스트로 토큰 검사할 수 있는 컨트롤러
    
    * add: api spec
    
    * up: 로그인 사용자 반환값 변경
    
    * add: 토큰 값을 검사하는 res 추가
    
    * fix: 항상 이미지 pull 하도록 수정 (#4)
    
    * up: 사용하지 않는 의존성을 제거
    
    * add: 프로그램 버전 명 추가
    
    * up: 안내메시지 친화적으로 변경
    
    * up: 리소스 리미트 추가
    
    * docs: 설명 추가
    
    * up: 웹 서버 상태를 알 수 있는 기본 컨트롤러 추가

--- controller/login.go
+++ controller/login.go
@@ -8,12 +8,10 @@ import (
 
 func Login(c *gin.Context) {
 	var msg struct {
-		Name    string `json:"user"`
-		Message string
-		Number  int
+		Id   string `json:"id"`
+		Name string `json:"name"`
 	}
-	msg.Name = "Lena"
-	msg.Message = "hey"
-	msg.Number = 123
+	msg.Id = "2d63c985-6745-9d20-7470-fe025dc2a6b0"
+	msg.Name = "Kyungeun"
 	c.JSON(http.StatusOK, msg)
 }
