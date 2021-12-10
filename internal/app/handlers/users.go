cfe785f094a6259ae7793a4134ab958403189ef4
박경은 <ruddms936@naver.com>
2021-12-11 06:25:58 +0900

    fix: 카카오톡 소셜 테이블 삭제 로직

--- internal/app/handlers/users.go
+++ internal/app/handlers/users.go
@@ -568,15 +568,24 @@ func UserWithdrawal(c *gin.Context) {
 		return
 	}
 
-	// 카카오 테이블만 삭제하더라도 reference된 사용자 데이블도 cascade로 같이 삭제되지만 카카오 로그인이 아닌 회원가입의 경우 삭제되지 않기 때문에 2번의 쿼리로 사용자 데이터를 삭제하도록 합니다.
-	if err := orm.Client.Where("email = ?", au.UserId).Delete(&entitys.User{}).Error; err != nil {
+	// jwt claim에는 user table의 id만 존재하여 카카오톡 소셜 테이블을 삭제하기 위한 정보를 얻기 위해 조회
+	user := entitys.User{}
+	if err := orm.Client.Where("id = ?", au.UserId).Find(&user).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, models.ErrResponse{
 			Message: err.Error(),
 		})
 		return
 	}
 
-	if err := orm.Client.Where("email = ?", au.UserId).Delete(&entitys.KakaoTalkSocial{}).Error; err != nil {
+	// 카카오톡 소셜 테이블만 삭제하더라도 reference된 사용자 데이블도 cascade로 같이 삭제되지만 카카오 로그인이 아닌 회원가입의 경우 삭제되지 않기 때문에 2번의 쿼리로 사용자 데이터를 삭제하도록 합니다.
+	if err := orm.Client.Where("id = ?", au.UserId).Delete(&entitys.User{}).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, models.ErrResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	if err := orm.Client.Where("id = ?", user.KakaoTalkSocialsId).Delete(&entitys.KakaoTalkSocial{}).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, models.ErrResponse{
 			Message: err.Error(),
 		})
