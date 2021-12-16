a1ead2614200c553ebdd42af0fe55f07a542cb05
박경은 <ruddms936@naver.com>
2021-12-16 18:11:05 +0900

    up: 고화질의 프로필 사진을 저장할 수 있도록 저장하는 url 변경

--- internal/app/models/kakao-user-information.go
+++ internal/app/models/kakao-user-information.go
@@ -11,6 +11,7 @@ type kakaoAccount struct {
 	Profile profile `json:"profile"`
 }
 
+// 프로필 사진의 경우 kakao_account.profile 과 properties 2곳에서 조회가 가능한데 가장 fresh한 데이터는 kakao_account.profile 에 존재합니다. properties 에서 프로필 사진 사용시 이전 프로필 사진이 사용될 수 있습니다.
 type profile struct {
 	Nickname          string `json:"nickname"`
 	ProfileImageUrl   string `json:"profile_image_url"`
