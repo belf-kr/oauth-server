74b55780c7a6457fabd7beb55a2eea058812cd33
Kyungeun Park <63892989+parkgang@users.noreply.github.com>
2021-12-17 00:49:26 +0900

    사용자 아바타 사진의 경우 고화질의 카카오톡 프로필 사진을 사용하도록 변경, k8s 스케일 업 (#31)
    * formatter: 코드 인덴트
    
    * up: 고화질의 프로필 사진을 저장할 수 있도록 저장하는 url 변경
    
    * up: 고화질의 이미지 처리시 느려지는 현상을 파악하여 스케일 업
    
    * up: api spec
    
    * up: 프로그램 버전 업

--- internal/app/entitys/kakao-talk-social.go
+++ internal/app/entitys/kakao-talk-social.go
@@ -4,5 +4,6 @@ type KakaoTalkSocial struct {
 	Id                uint   `gorm:"primary_key;" json:"id"`
 	Email             string `gorm:"unique;not null;size:50;" json:"email"`
 	NickName          string `gorm:"not null;size:20;" json:"nickName"`
-	ThumbnailImageUrl string `gorm:"size:2083;" json:"thumbnailImageUrl"` // 이미지의 url을 저장하기 위한 최대 크기로 잡아놓았습니다.
+	ProfileImageUrl   string `gorm:"size:2083;" json:"profileImageUrl"`   // 카카오톡에 실제로 저장된 프로필 사진 (고화질), 이미지의 url을 저장하기 위한 최대 크기로 잡아놓았습니다.
+	ThumbnailImageUrl string `gorm:"size:2083;" json:"thumbnailImageUrl"` // 카카오톡에 저장된 프로필 사진 미리보기 (저화질), 이미지의 url을 저장하기 위한 최대 크기로 잡아놓았습니다.
 }
